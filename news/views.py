203072e62aa8c51b64f091fc63b33ab4907c72b0
Zhanibek <smagul.zhanibek@gmail.com>
2018-09-14 17:55:08 +0600

    Building a lookup Form

--- news/views.py
+++ news/views.py
@@ -3,13 +3,20 @@ import json
 
 from django.shortcuts import render
 
+
 def home(request):
     # Grab Crypto Price Data
-    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,XLM,LTC,USDT,XMR,ADA,MIOTA&tsyms=USD")
+    price_request = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,XLM,LTC,USDT,XMR,ADA,MIOTA&tsyms=USD")
     price = json.loads(price_request.content)
 
     # Grab Crypto News
-    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api_request = requests.get(
+        "https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
 
     return render(request, 'home.html', {'api': api, 'price': price})
+
+
+def prices(request):
+    return render(request, 'prices.html', {})
